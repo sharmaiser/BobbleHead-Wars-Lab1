1f7997adfcc8872da9e482cc137e424cc70b2d59
Bhavay Sharma [Student] <bhavay.sharma@student.sl.on.ca>
2019-09-16 16:21:53 -0400

    Commit 4: Destroying Old Objects

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
