b749a6f9ae54bc74c6164e1423bee842cebbffb0
sharmaiser <54967627+sharmaiser@users.noreply.github.com>
2019-09-18 20:14:07 -0400

    RayCasting Added
    Gun Shoots in different directions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     public Rigidbody head;
     // Start is called before the first frame update
@@ -24,6 +26,7 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+
         //This code moves the head when marine moves.
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
@@ -38,5 +41,32 @@ public class PlayerController : MonoBehaviour
             //Direction and Force amount are multiplied
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
+        //Creating an empty ray
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //This will draw ray in scene
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+        
+        //Ray actually cast rays and layer mask lets cast know you are tring to hit
+        if (Physics.Raycast(ray, out hit, 1000, layerMask,
+                            QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+
+            // You get the target position 
+            Vector3 targetPosition = new Vector3(hit.point.x,
+            transform.position.y, hit.point.z);
+            // Calculate the quaternion for the rotation
+            Quaternion rotation = Quaternion.LookRotation(targetPosition -
+            transform.position);
+            // Using Lerp() for smoother rotation for our Marine 
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+            rotation, Time.deltaTime * 10.0f);
+        }
+
     }
 }
